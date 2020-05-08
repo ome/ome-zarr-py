7acebef41f06ba01ab86c75f0c1af9d6a9aa7537
jmoore <josh@glencoesoftware.com>
2020-05-08 12:45:48 +0200

    Bump version: 0.0.3.dev0 → 0.0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires += ['vispy'],
 
 setup(
     name='ome-zarr',
-    version='0.0.3.dev0',
+    version='0.0.3',
     author='The Open Microscopy Team',
     url='https://github.com/ome/ome-zarr-py',
     description='Implementation of images in Zarr files.',
