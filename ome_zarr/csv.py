008a8b2215d401fa83b4358be34d133b6cb8aabf
William Moore <w.moore@dundee.ac.uk>
2020-11-19 11:08:40 +0000

    Handle csv column types

--- ome_zarr/csv.py
+++ ome_zarr/csv.py
@@ -1,10 +1,31 @@
 import csv
 import os
-from typing import Dict
+from typing import Dict, Union
 
 from zarr.convenience import open as zarr_open
 
 from .io import parse_url
+
+# d: DoubleColumn, for floating point numbers
+# l: LongColumn, for integer numbers
+# s: StringColumn, for text
+# b: BoolColumn, for true/false
+COLUMN_TYPES = ["d", "l", "s", "b"]
+
+
+def parse_csv_value(value: str, col_type: str) -> Union[str, float, int, bool]:
+    """Parse string value from csv, according to COLUMN_TYPES"""
+    rv: Union[str, float, int, bool] = value
+    try:
+        if col_type == "d":
+            rv = float(value)
+        elif col_type == "l":
+            rv = int(round(float(value)))
+        elif col_type == "b":
+            rv = bool(value)
+    except ValueError:
+        pass
+    return rv
 
 
 def csv_to_zarr(
@@ -14,7 +35,16 @@ def csv_to_zarr(
     - URL of the form: path/to/ID.zarr/
     """
 
-    cols_to_read = csv_keys.split(",")
+    # Use #d to denote double etc.
+    # e.g. "area (pixels)#d,well_label#s,Width#l,Height#l"
+    cols_types_by_name: Dict[str, str] = {}
+    for col_name_type in csv_keys.split(","):
+        if "#" in col_name_type:
+            col_name, col_type = col_name_type.rsplit("#", 1)
+            col_type = col_type if col_type in COLUMN_TYPES else "s"
+            cols_types_by_name[col_name] = col_type
+        else:
+            cols_types_by_name[col_name_type] = "s"
 
     csv_columns = None
     id_column = None
@@ -33,13 +63,14 @@ def csv_to_zarr(
                         f"csv column name: {csv_columns}"
                     )
                 id_column = csv_columns.index(csv_id)
-                print("id_column", id_column)
             else:
                 row_id = row[id_column]
                 row_props = {}
                 for col_name, value in zip(csv_columns, row):
-                    if col_name in cols_to_read:
-                        row_props[col_name] = value
+                    if col_name in cols_types_by_name:
+                        row_props[col_name] = parse_csv_value(
+                            value, cols_types_by_name[col_name]
+                        )
                 props_by_id[row_id] = row_props
 
     dict_to_zarr(props_by_id, zarr_path, zarr_id)
@@ -48,22 +79,20 @@ def csv_to_zarr(
 def dict_to_zarr(props_to_add: Dict, zarr_path: str, zarr_id: str) -> None:
 
     zarr = parse_url(zarr_path)
-    print("zarr", zarr)
     if not zarr:
-        print(f"No zarr found at {zarr_path}")
-        return
+        raise Exception(f"No zarr found at {zarr_path}")
 
-    print(zarr.root_attrs)
     plate_attrs = zarr.root_attrs.get("plate", None)
     if plate_attrs is None:
-        print(f"zarr_path must be to plate.zarr. No 'plate' in {zarr_path}.zattrs")
+        raise Exception(
+            f"zarr_path must be to plate.zarr. No 'plate' in {zarr_path}.zattrs"
+        )
     well_paths = [w["path"] for w in plate_attrs.get("wells", [])]
 
     # look for 'label/0' under the first field of each Well
     field = "0"
     for well in well_paths:
         path_to_labels = os.path.join(zarr_path, well, field, "labels", "0")
-        print("path_to_labels", path_to_labels)
 
         label_group = zarr_open(path_to_labels)
         attrs = label_group.attrs.asdict()
@@ -71,12 +100,8 @@ def dict_to_zarr(props_to_add: Dict, zarr_path: str, zarr_id: str) -> None:
         if properties is None:
             continue
 
-        print("attrs", attrs)
-        attrs["test"] = True
-
         for props_dict in properties:
             props_id = str(props_dict.get(zarr_id))
-            print("props_id", props_id)
 
             # Look for key-value pairs to add to these properties
             if props_id in props_to_add:
