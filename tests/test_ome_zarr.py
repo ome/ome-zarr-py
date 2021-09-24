b038431a9ca71eb72b5cc46cc5b63c813e47b8d3
William Moore <w.moore@dundee.ac.uk>
2021-09-24 17:37:51 +0100

    Update test_ome_zarr.py to expect different shapes from data.create_zarr()

--- tests/test_ome_zarr.py
+++ tests/test_ome_zarr.py
@@ -7,10 +7,10 @@ from ome_zarr.data import astronaut, create_zarr
 from ome_zarr.utils import download, info
 
 
-def log_strings(idx, t, c, z, y, x, ct, cc, cz, cy, cx, dtype):
+def log_strings(idx, c, y, x, cc, cy, cx, dtype):
     yield f"resolution: {idx}"
-    yield f" - shape ('t', 'c', 'z', 'y', 'x') = ({t}, {c}, {z}, {y}, {x})"
-    yield f" - chunks =  ['{ct}', '{cc}', '{cz}', '{cx}', '{cy}']"
+    yield f" - shape ('c', 'y', 'x') = ({c}, {y}, {x})"
+    yield f" - chunks =  ['{cc}', '{cx}', '{cy}']"
     yield f" - dtype = {dtype}"
 
 
@@ -21,9 +21,9 @@ class TestOmeZarr:
         create_zarr(str(self.path), method=astronaut)
 
     def check_info_stdout(self, out):
-        for log in log_strings(0, 1, 3, 1, 1024, 1024, 1, 1, 1, 256, 256, "float64"):
+        for log in log_strings(0, 3, 1024, 1024, 1, 64, 64, "uint8"):
             assert log in out
-        for log in log_strings(1, 1, 3, 1, 512, 512, 1, 1, 1, 256, 256, "float64"):
+        for log in log_strings(1, 3, 512, 512, 1, 64, 64, "int8"):
             assert log in out
 
         # from info's print of omero metadata
