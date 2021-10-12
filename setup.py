6134152f1055142203f92d3ca980364ca6d455ed
Sebastien Besson <seb.besson@gmail.com>
2021-10-12 12:26:22 +0100

    Bump version: 0.1.2.dev0 → 0.2.0.dev0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires += (["toolz"],)
 
 setup(
     name="ome-zarr",
-    version="0.1.2.dev0",
+    version="0.2.0.dev0",
     author="The Open Microscopy Team",
     url="https://github.com/ome/ome-zarr-py",
     description="Implementation of images in Zarr files.",
