e15605d5fe27b1b70198ef528494c22c83709d65
Sebastien Besson <seb.besson@gmail.com>
2022-03-26 09:05:52 +0000

    Try setting up intersphinx for cross-linking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@ sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
 ]
 
 # use index.rst instead of contents.rst
@@ -18,3 +19,11 @@ master_doc = "index"
 project = "ome-zarr-py"
 copyright = "Open Microscopy Environment"  # noqa: A001
 author = "Open Microscopy Environment"
+
+# Example configuration for intersphinx: refer to the Python standard library.
+# use in refs e.g:
+# :ref:`comparison manual <python:comparisons>`
+intersphinx_mapping = {
+    'python':('https://docs.python.org/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'zarr': ('https://zarr.readthedocs.io/en/stable/', None)}
