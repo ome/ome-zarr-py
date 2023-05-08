1493e3f3bbd13cf4e5fc9b3fa2da79c20c33eca6
Josh Moore <josh@openmicroscopy.org>
2023-05-08 10:47:19 +0200

    Bump version: 0.7.1.dev0 → 0.7.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires += (["toolz"],)
 
 setup(
     name="ome-zarr",
-    version="0.7.1.dev0",
+    version="0.7.1",
     author="The Open Microscopy Team",
     url="https://github.com/ome/ome-zarr-py",
     description="Implementation of images in Zarr files.",
