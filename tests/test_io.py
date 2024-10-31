19b89a8e1529265eaca88788f83a7c0417526456
William Moore <w.moore@dundee.ac.uk>
2024-10-31 16:32:15 +0000

    Passing all 6 test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import fsspec
 import pytest
 import zarr
+from zarr.storage import LocalStore
 
 from ome_zarr.data import create_zarr
 from ome_zarr.io import ZarrLocation, parse_url
@@ -13,7 +14,8 @@ class TestIO:
     def initdir(self, tmpdir):
         self.path = tmpdir.mkdir("data")
         create_zarr(str(self.path))
-        self.store = parse_url(str(self.path), mode="w").store
+        # this overwrites the data if mode="w"
+        self.store = parse_url(str(self.path), mode="r").store
         self.root = zarr.group(store=self.store)
 
     def test_parse_url(self):
@@ -32,7 +34,6 @@ class TestIO:
         assert ZarrLocation(self.store)
 
     def test_loc_fs(self):
-        fs = fsspec.filesystem("memory")
-        fsstore = zarr.storage.FSStore(url="/", fs=fs)
-        loc = ZarrLocation(fsstore)
+        store = LocalStore(str(self.path))
+        loc = ZarrLocation(store)
         assert loc
