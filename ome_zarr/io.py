8fc02b4293e751a0a922c58190317b34e90bf2b3
William Moore <w.moore@dundee.ac.uk>
2024-11-07 14:54:09 +0000

    Remove unused imports

--- ome_zarr/io.py
+++ ome_zarr/io.py
@@ -3,7 +3,6 @@
 Primary entry point is the :func:`~ome_zarr.io.parse_url` method.
 """
 
-import json
 import logging
 from pathlib import Path
 from typing import List, Optional, Union
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import fsspec
 import pytest
 import zarr
 from zarr.storage import LocalStore
