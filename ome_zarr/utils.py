6048cea973634922b1113f805d9365b7c4743528
William Moore <w.moore@dundee.ac.uk>
2025-02-13 22:21:11 +0000

    ome_zarr view opens BioFile-Finder for multiple zarrs

--- ome_zarr/utils.py
+++ ome_zarr/utils.py
@@ -1,9 +1,12 @@
 """Utility methods for ome_zarr access."""
 
+import csv
 import json
 import logging
 import os
+import urllib
 import webbrowser
+import xml.etree.ElementTree as ET
 from collections.abc import Iterator
 from http.server import (  # type: ignore[attr-defined]
     HTTPServer,
@@ -52,11 +55,138 @@ def info(path: str, stats: bool = False) -> Iterator[Node]:
         yield node
 
 
+def find_multiscales(path_to_zattrs):
+    # return list of images. Each image is [path_to_zarr, name, dirname]
+    # We want full path to find the multiscales Image. e.g. full/path/to/image.zarr/0
+    # AND we want image Name, e.g. "image.zarr Series 0"
+    # AND we want the dir path to use for Tags e.g. full/path/to
+    with open(path_to_zattrs / ".zattrs") as f:
+        text = f.read()
+    zattrs = json.loads(text)
+    if "plate" in zattrs:
+        plate = zattrs.get("plate")
+        wells = plate.get("wells")
+        field = "0"
+        print("well", wells[0])
+        path_to_zarr = path_to_zattrs / wells[0].get("path") / field
+        plate_name = os.path.basename(path_to_zattrs)
+        return [[path_to_zarr, plate_name, os.path.dirname(path_to_zattrs)]]
+    elif zattrs.get("bioformats2raw.layout") == 3:
+        # Open OME/METADATA.ome.xml
+        print("bioformats2raw.layout...")
+        try:
+            tree = ET.parse(path_to_zattrs / "OME" / "METADATA.ome.xml")
+            root = tree.getroot()
+            # spec says "If the "series" attribute does not exist and no "plate" is
+            # present, separate "multiscales" images MUST be stored in consecutively
+            # numbered groups starting from 0 (i.e. "0/", "1/", "2/", "3/", ...)."
+            series = 0
+            images = []
+            for child in root:
+                # tag is eg. {http://www.openmicroscopy.org/Schemas/OME/2016-06}Image
+                if child.tag.endswith("Image"):
+                    img_name = (
+                        os.path.basename(path_to_zattrs) + " Series:" + str(series)
+                    )
+                    # Get Name from XML metadata, otherwise use path and Series
+                    img_name = child.attrib.get("Name", img_name)
+                    images.append(
+                        [
+                            path_to_zattrs / str(series),
+                            img_name,
+                            os.path.dirname(path_to_zattrs),
+                        ]
+                    )
+                    series += 1
+            return images
+        except Exception as ex:
+            print(ex)
+    elif zattrs.get("multiscales"):
+        return [
+            [
+                path_to_zattrs,
+                os.path.basename(path_to_zattrs),
+                os.path.dirname(path_to_zattrs),
+            ]
+        ]
+    return []
+
+
+def splitall(path):
+    # Use os.path.split() repeatedly to split path into dirs
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path:  # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
+
 def view(input_path: str, port: int = 8000) -> None:
     # serve the parent directory in a simple server with CORS. Open browser
-
     parent_dir, image_name = os.path.split(input_path)
     parent_dir = str(parent_dir)
+
+    # walk the input path to find all .zattrs files...
+    def walk(path: Path):
+        print("walk", path, (path / ".zattrs").exists())
+        if (path / ".zattrs").exists():
+            yield from find_multiscales(path)
+        else:
+            for p in path.iterdir():
+                if (p / ".zattrs").exists():
+                    yield from find_multiscales(p)
+                elif p.is_dir():
+                    yield from walk(p)
+                else:
+                    continue
+
+    zarrs = list(walk(Path(input_path)))
+
+    for z in zarrs:
+        # split file path into list
+        z[2] = splitall(z[2])
+
+    # If we have just one zarr, open ome-ngff-validator in a web browser...
+    if len(zarrs) == 1:
+        url = (
+            f"https://ome.github.io/ome-ngff-validator/"
+            f"?source=http://localhost:{port}/{image_name}"
+        )
+    elif len(zarrs) > 1:
+        # ...otherwise write to CSV file and open in BioFile Finder
+        max_folders = max(len(z[2]) for z in zarrs)
+        col_names = ["File Path", "File Name"] + [
+            f"Folder {i}" for i in range(max_folders)
+        ]
+        # open csv file and write lines...
+        bff_csv = os.path.join(input_path, "biofile_finder.csv")
+        with open(bff_csv, "w", newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+            writer.writerow(col_names)
+            for zarr_img in zarrs:
+                # path_to_zarr, name, dirname
+                file_path = f"http://localhost:{port}/{zarr_img[0]}"
+                name = zarr_img[1]
+                # folders list needs to be same length for every row.
+                # e.g. ['f1', 'f2', '-', '-']
+                folders = zarr_img[2] + ["-"] * (max_folders - len(zarr_img[2]))
+                writer.writerow([file_path, name] + folders)
+
+        source = {
+            "uri": f"http://localhost:{port}/{image_name}/biofile_finder.csv",
+            "type": "csv",
+            "name": "biofile_finder.csv",
+        }
+        s = urllib.parse.quote(json.dumps(source))
+        url = f"https://bff.allencell.org/app?source={s}"
 
     class CORSRequestHandler(SimpleHTTPRequestHandler):
         def end_headers(self) -> None:
@@ -70,11 +200,7 @@ def view(input_path: str, port: int = 8000) -> None:
             super_path = super().translate_path(path)
             return super_path
 
-    # open ome-ngff-validator in a web browser...
-    url = (
-        f"https://ome.github.io/ome-ngff-validator/"
-        f"?source=http://localhost:{port}/{image_name}"
-    )
+    # Open in browser...
     webbrowser.open(url)
 
     # ...then start serving content
