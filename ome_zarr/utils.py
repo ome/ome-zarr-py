a0e488a8d805717cd7fc6700e7b4f732cd2d9e3f
William Moore <w.moore@dundee.ac.uk>
2025-10-09 10:15:01 +0100

    ome_zarr view doesn't try to check for images

--- ome_zarr/utils.py
+++ ome_zarr/utils.py
@@ -77,18 +77,6 @@ def view(input_path: str, port: int = 8000, dry_run: bool = False) -> None:
     # serve the parent directory in a simple server with CORS. Open browser
     # dry_run is for testing, so we don't open the browser or start the server
 
-    zarrs = []
-    if (Path(input_path) / ".zattrs").exists() or (
-        Path(input_path) / "zarr.json"
-    ).exists():
-        zarrs = find_multiscales(Path(input_path))
-    if len(zarrs) == 0:
-        print(
-            f"No OME-Zarr images found in {input_path}. "
-            f"Try $ ome_zarr finder {input_path}"
-        )
-        return
-
     parent_dir, image_name = os.path.split(input_path)
     if len(image_name) == 0:
         parent_dir, image_name = os.path.split(parent_dir)
