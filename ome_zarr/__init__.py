3bdda3745c2c9b8da6f3cf2fa1af7cdb1880eeb8
Nirkan <kandpalniraj88@gmail.com>
2025-10-30 14:24:49 +0100

    Add __version__ fallback to fix RTD build

--- ome_zarr/__init__.py
+++ ome_zarr/__init__.py
@@ -1,3 +1,9 @@
-from ._version import version as __version__
+# Expose __version__ and fallback when _version.py doesn't exist.
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0+unknown"
+
 
 __all__ = ["__version__"]
