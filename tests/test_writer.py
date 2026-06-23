bb85dc514d4b5d55ec8307bb0948dd44f345e1be
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-06-23 22:30:23 +0200

    tests: test that version is actually written

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -176,7 +176,10 @@ class TestWriter:
             coordinate_systems=additional_cs,
             default_coordinate_system_name="physical",
         )
-        ms.to_ome_zarr(self.path / "test_transforms.zarr", version="0.6.dev4")
+        ms.to_ome_zarr(
+            zarr.open(self.path / "test_transforms.zarr", mode="w"),
+            version="0.6.dev4",
+            overwrite=True)
 
         # make transform go bad
         additional_transforms["output"]["name"] = "nonexistent"
@@ -191,6 +194,42 @@ class TestWriter:
                 coordinate_systems=additional_cs,
                 default_coordinate_system_name="physical",
             )
+
+    @pytest.mark.parametrize(
+        "version", ("0.4", "0.5", "0.6.dev4"), ids=["V04", "V05", "V06"]
+    )
+    def test_image_class_versions(self, version):
+        data = self.create_data((2, 128, 128))
+        image = OMEZarrImage(data=data, axes="cyx", scale={"y": 0.5, "x": 0.5})
+        ms = OMEZarrMultiscale(
+            image=image,
+        )
+        if version == "0.4":
+            grp = zarr.open(
+                self.path / f"test_versions_{version}.zarr",
+                mode="w",
+                zarr_format=2,
+                )
+        else:
+            grp = zarr.open(
+                self.path / f"test_versions_{version}.zarr",
+                mode="w",
+                zarr_format=3,
+                )
+        ms.to_ome_zarr(
+            grp,
+            overwrite=True,
+            version=version
+            )
+        
+        # open the written zarr and check the version
+        out = zarr.open_group(self.path / f"test_versions_{version}.zarr")
+        if version == "0.4":
+            metadata = out.attrs["multiscales"][0]
+        else:
+            metadata = out.attrs.get("ome", {})
+        
+        assert metadata["version"] == version
 
     def test_image_class_bad_args(self):
         data = self.create_data((2, 128, 128))
