24e8b94613d463c865b581aad3ae103216e91d0b
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-07-01 13:53:29 +0200

    feat: default version should be 0.5

--- ome_zarr/classes/image.py
+++ ome_zarr/classes/image.py
@@ -343,7 +343,7 @@ class OMEZarrMultiscaleBase:
         self,
         group: zarr.Group | str,
         storage_options: list[dict[str, Any]] | dict[str, Any] | None = None,
-        version: Literal["0.6.dev4", "0.5", "0.4"] = "0.6.dev4",
+        version: Literal["0.6.dev4", "0.5", "0.4"] = "0.5",
         compute: bool = True,
         overwrite: bool = False,
     ) -> list:
