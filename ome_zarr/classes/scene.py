af6bb47660acf6bf891482567d10d30b83dde20a
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-09-21 17:26:50 +0100

    Remove square affine special case

--- ome_zarr/classes/scene.py
+++ ome_zarr/classes/scene.py
@@ -487,12 +487,7 @@ class OMEZarrScene:
                     "Path-form transformations are not implemented", transform
                 ) from e
 
-            if aff.shape[0] == aff.shape[1]:
-                return tnd.transforms.Affine(aff)
-            else:
-                aff2 = np.eye(max(aff.shape))
-                aff2[: aff.shape[0], : aff.shape[1]] = aff
-                return tnd.transforms.Affine(aff2)
+            return tnd.transforms.Affine.from_linear_map(aff[:, :-1], aff[:, -1])
 
         elif isinstance(transform, ozmt.Coordinates):
             arr, index_transform, vector_axis = self._setup_vectorfield_args(
